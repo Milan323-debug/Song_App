d08f92789c5c9966298bd6a6426aef4eff9293ff
Milan323-debug <milanpramanik2024@gmail.com>
2025-10-06 22:58:52 +0530

    fix middleware

---
+++ backend/src/middleware/upload.middleware.js
@@ -0,0 +1,30 @@
+// multer is a middleware for handling multipart/form-data, which is primarily used for file uploads
+
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  // allow image and video mime types
+  // allow image, video and audio mime types
+  if (
+    file.mimetype.startsWith("image/") ||
+    file.mimetype.startsWith("video/") ||
+    file.mimetype.startsWith("audio/")
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image or video files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 4 * 1024 * 1024, // 4MB limit to stay within Vercel's free tier limit
+    files: 1 // Only allow 1 file per request
+  },
+});
+
+export default upload;
